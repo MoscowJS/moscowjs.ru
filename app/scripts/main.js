c98ef46184f90cab0f99621bdf3ecdd5621b1681
filipovsky <filipovskii.off@gmail.com>
2014-10-06 17:58:16 +0400

    Template fixes.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,7 @@
 'use strict';
 
 $(function () {
+  var iframeWidth = $(window).width() * 0.9;
   Parse.initialize(
     'dz8YZ3MDQKztmmHFa4hlWwxeI01TASxYQcQbIi0Z',
     'WLzUuXD8laIM8br5WGalwy2Z0u7x4J8zZxJ2y6RW');
@@ -29,4 +30,7 @@ $(function () {
       })
       .done();
   });
+
+  $('iframe').attr('width', iframeWidth);
+  $('iframe').attr('height', 0.5625 * iframeWidth);
 });
