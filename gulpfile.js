701cc631c9905fe8faa1fd61ab1f5e9b329ba1c9
filipovsky <filipovskii.off@gmail.com>
2015-01-21 11:50:11 +0100

    MoscowJS 18.

--- gulpfile.js
+++ gulpfile.js
@@ -94,14 +94,14 @@ gulp.task('connect', function () {
         .use(connect.directory('app'));
 
     require('http').createServer(app)
-        .listen(9000)
+        .listen(8888)
         .on('listening', function () {
-            console.log('Started connect web server on http://localhost:9000');
+            console.log('Started connect web server on http://localhost:8888');
         });
 });
 
 gulp.task('serve', ['gen', 'connect', 'styles'], function () {
-    require('opn')('http://localhost:9000');
+    require('opn')('http://localhost:8888');
 });
 
 // inject bower components
